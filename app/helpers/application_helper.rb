b135841b5767074c94f3f3fcb1c43b63a44e10b3
phuonghusthn <nguyenphuongnt1997@gmail.com>
2019-10-18 08:43:18 +0700

    Chapter4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title
+    base_title = I18n.t "ruby_on_rails_tutorial_sample_app"
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
